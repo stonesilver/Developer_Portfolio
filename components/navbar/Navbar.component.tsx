6378ab20dbbd4c5ed5822326ec375e6447e689f6
Ezenwa Chigozie <stonesilver12@gmail.com>
2021-06-12 13:57:36 +0100

    worked on the navbar responsiveness. Also added the mobile hamburger menu and some animations

--- components/navbar/Navbar.component.tsx
+++ components/navbar/Navbar.component.tsx
@@ -8,13 +8,35 @@ const NavBar = () => {
           <span className={navbarStyles.code}>&#8725;</span>
           <span className={navbarStyles.code}>&#62;</span>
         </div>
+        <span className={navbarStyles.name}>ESC</span>
       </div>
       <div className={navbarStyles.navbarLinksContainer}>
+        <input
+          type='checkbox'
+          name='mobileToggle'
+          id={navbarStyles.mobileToggle}
+        />
+        <div className={navbarStyles.mobileToggle}>
+          <div className={navbarStyles.hamburgerContainer}>
+            <span className={navbarStyles.hambuger}></span>
+            <span className={navbarStyles.hambuger}></span>
+            <span className={navbarStyles.hambuger}></span>
+          </div>
+        </div>
         <ul className={navbarStyles.navLinks}>
-          <li className={navbarStyles.link}><a href="#about">About</a></li>
-          <li className={navbarStyles.link}><a href="#works">Works</a></li>
-          <li className={navbarStyles.link}><a href="#services">Services</a></li>
-          <li className={navbarStyles.link}><a href="#contact">Contact</a></li>
+          <li className={navbarStyles.link}>
+            <a href='#about'>About</a>
+          </li>
+          <li className={navbarStyles.link}>
+            <a href='#works'>Works</a>
+          </li>
+          <li className={navbarStyles.link}>
+            <a href='#services'>Services</a>
+          </li>
+          <li className={navbarStyles.link}>
+            <a href='#contact'>Contact</a>
+          </li>
+          <li className={navbarStyles.link}>theme toggle</li>
         </ul>
       </div>
     </nav>
