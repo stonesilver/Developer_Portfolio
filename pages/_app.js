e1372a5bdcdd87183f3496d56363a2954e9236e3
Ezenwa Chigozie <stonesilver12@gmail.com>
2021-08-30 17:14:12 +0100

    added imported font awesome styles in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
 import Layout from '../components/Layout/Layout.component';
 import NavBar from '../components/navbar/Navbar.component';
 import Footer from '../components/Footer/Footer.component'
 import '../styles/globals.scss';
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
   return (
