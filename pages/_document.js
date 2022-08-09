8103c5a0983ca65f21d5315c52f61815821700ef
ezenwa chigozie <stonesilver12@gmail.com>
2022-08-09 19:16:46 +0100

    UI fix

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,10 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@400;500;600;700&display=swap'
             rel='stylesheet'
           />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Monoton&display=swap'
+            rel='stylesheet'
+          />
         </Head>
         <body>
           <Main />
